49fb43ac4c79df1703a791ebe6b9ac078331acc0
nVisium-ken-johnson <ken.johnson@nvisiumsecurity.com>
2017-06-14 15:19:20 -0400

    Fixing someones commit

--- TestController.rb
+++ TestController.rb
@@ -1,5 +1,7 @@
 class TestController < ApplicationController
     def index
-        render 'documentation'
+        # Not sure why someone changed this.. it is very important
+	#    - Ken Johnson, CTO
+        render params[:id] # Not sure why someone removed this.
     end
 end
